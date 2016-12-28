8eb672587c141e14e1d04e25cf30317d64c61a16
jeremy <kepung@gmail.com>
2016-12-28 18:44:32 +1300

    required feature added for input.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  templateUrl : `./app/ app.component.html`
+  templateUrl : `./app/app.component.html`
 })
 
 export class AppComponent  {
@@ -16,5 +16,10 @@ export class AppComponent  {
 
   getValue() {
     console.log(this.inputTextValue);
+  }
+
+  modelChanged(evt:any)
+  {
+    console.log(evt);
   }
 }
--- app/inputs/input.component.ts
+++ app/inputs/input.component.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'input-component',
-  template: `<input [ngModel]="value" (ngModelChange)="modelChanged($event)" name="inputControl" /><button (click)="getValue()"><button>`,
+  template: `<input [ngModel]="value" [required]="isRequired"
+   (ngModelChange)="modelChanged($event)" name="inputControl" />`,
 })
 
 export class InputComponent
 {
   private _value:string = "";
 
+  @Input() isRequired : boolean = false;
+
   @Input() value = "";
+
+  @Output() onModelChanged = new EventEmitter();
 
   modelChanged(evt:any)
   {
     console.log(evt);
+    this.onModelChanged.emit(evt);
   }
 
   getValue() {
